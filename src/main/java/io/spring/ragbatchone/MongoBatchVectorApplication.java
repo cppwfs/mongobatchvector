b374d217071fb41c70b50eedaa3ebcfcd074a224
Glenn Renfro <grenfro@vmware.com>
2024-09-20 11:00:58 -0400

    Provide sample for writing to a mongodb vectorstore using batch

---
+++ src/main/java/io/spring/ragbatchone/MongoBatchVectorApplication.java
@@ -0,0 +1,13 @@
+package io.spring.ragbatchone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoBatchVectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoBatchVectorApplication.class, args);
+	}
+
+}
---
+++ src/main/java/io/spring/ragbatchone/MongoBatchVectorConfiguration.java
@@ -0,0 +1,63 @@
+package io.spring.ragbatchone;
+
+import io.spring.aibatchtools.TikaItemReaderBuilder;
+import io.spring.aibatchtools.VectorStoreWriterBuilder;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.task.configuration.EnableTask;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+@EnableTask
+public class MongoBatchVectorConfiguration {
+
+    @Value("classpath:/docs/depdoc.pdf")
+    private Resource textResource;
+
+    @Bean
+    public ItemReader reader() {
+        return new TikaItemReaderBuilder().name("PDFReader").
+                textSplitter(new TokenTextSplitter()).
+                resource(textResource).
+                build();
+    }
+
+    @Bean
+    public ItemWriter writer(VectorStore vectorStore) {
+        return new VectorStoreWriterBuilder().
+                contentFieldName("message").
+                metaDataFieldName("keyData").
+                vectorStore(vectorStore).build();
+    }
+
+    @Bean
+    public Job processDocumentsJob(JobRepository jobRepository, Step step1) {
+        return new JobBuilder("importDocJob", jobRepository)
+                .incrementer(new RunIdIncrementer())
+                .flow(step1)
+                .end()
+                .build();
+    }
+
+    @Bean
+    public Step step1(JobRepository jobRepository, PlatformTransactionManager transactionManager, VectorStore vectorStore) {
+        return new StepBuilder("step1", jobRepository)
+                .chunk(10, transactionManager)
+                .reader(reader())
+                .writer(writer(vectorStore))
+                .build();
+    }
+
+}
